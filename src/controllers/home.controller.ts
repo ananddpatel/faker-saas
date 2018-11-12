f98899f2547c7a8d0a6180eb07c2ccb02bf41766
Anand Patel <anandpatel3@hotmail.com>
2018-11-11 21:54:30 -0500

    showing method groups on frontend

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,9 +1,9 @@
 import * as faker from 'faker';
+import { fakerMethods } from '../data/faker-methods';
 
 export const index = (_req, res) => {
   const address = faker.address.city(1);
   console.log(address);
   
-
-  res.render("pages/index");
+  res.render("pages/index", {methods: fakerMethods});
 };
